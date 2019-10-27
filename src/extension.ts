ce36a4e68629d3cdd9a733de40040d7a543ab523
Yudu Ban <banyudu@gmail.com>
2019-10-27 20:00:57 +0800

    feat: support show notifications for fixed module and time

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import npmLatestVersions from 'npm-latest-versions'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,4 +14,13 @@ export function deactivate (): void {
   // nothing
 }
 
-console.log('hello world!')
+;
+setInterval(async () => {
+  const moduleName = 'express'
+  const versions = await npmLatestVersions({
+    module: moduleName,
+    since: new Date('2019/01/01')
+  })
+  const message = Object.keys(versions).map(tag => `${moduleName}@${versions[tag].version}`).join('\n')
+  vscode.window.showInformationMessage(message)
+}, 3 * 1000)
