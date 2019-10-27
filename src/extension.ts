5c3d43a5e312ecbe34802a46aa5bb7f447dbda9b
Yudu Ban <banyudu@gmail.com>
2019-10-27 20:20:39 +0800

    refactor: move code to activate/deactivate functions

--- src/extension.ts
+++ src/extension.ts
@@ -3,19 +3,9 @@
 import * as vscode from 'vscode'
 import npmLatestVersions from 'npm-latest-versions'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate (context: vscode.ExtensionContext): void {
-  // nothing
-}
+let timer: NodeJS.Timeout | undefined
 
-// this method is called when your extension is deactivated
-export function deactivate (): void {
-  // nothing
-}
-
-;
-setInterval(async () => {
+async function check () {
   const moduleName = 'express'
   const versions = await npmLatestVersions({
     module: moduleName,
@@ -23,4 +13,22 @@ setInterval(async () => {
   })
   const message = Object.keys(versions).map(tag => `${moduleName}@${versions[tag].version}`).join('\n')
   vscode.window.showInformationMessage(message)
-}, 3 * 1000)
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate (context: vscode.ExtensionContext): void {
+  console.log('activate')
+  // nothing
+  if (!timer) {
+    timer = setInterval(check, 3 * 1000)
+  }
+}
+
+// this method is called when your extension is deactivated
+export function deactivate (): void {
+  // nothing
+  console.log('deactivate')
+  clearInterval(timer!)
+  timer = undefined
+}
